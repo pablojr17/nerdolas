63219a940b3da49243a4edf3383fafbb968487a0
Pablo Gomes <pablojr17@gmail.com>
2023-11-29 12:11:57 -0300

    Criação estrutura basica

---
+++ script.js
@@ -0,0 +1,5 @@
+function redirectTo(route) {
+  // Adicione aqui a lógica para redirecionar para a rota desejada
+  console.log(`Redirecionando para ${route}`);
+  window.location.href = "/" + route;
+}
